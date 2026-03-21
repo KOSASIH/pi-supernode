05fe54eb0455ea20fb1e450584c4c34f74c281d9
KOSASIH <kosasihg88@gmail.com>
2026-03-21 14:35:28 +0700

    Update zk_threshold.go

--- pkg/crypto/zk_threshold.go
+++ pkg/crypto/zk_threshold.go
@@ -1,29 +1,106 @@
 package crypto
 
 import (
-    "github.com/arnaud-morini/zk"
-    bls "github.com/chai2010/bls12-381"
-    "github.com/ethereum/go-ethereum/crypto/bls12381"
+	"crypto/rand"
+	"errors"
+	"fmt"
+	"math/big"
+
+	"github.com/chai2010/bls12-381"
+	"github.com/ethereum/go-ethereum/crypto/bls12381"
+	"golang.org/x/crypto/sha3"
 )
 
-type ZKThreshold struct {
-    tks *bls.SecretKey
-    pk  *bls.PublicKey
+type ThresholdSig struct {
+	threshold int
+	total     int
+	secret    *bls.SecretKey
+	pubKey    *bls.PublicKey
+	shares    map[int]*bls.SecretKey
 }
 
-func NewZKThreshold(threshold int, total int) (*ZKThreshold, error) {
-    sk, err := bls.GenerateKey()
-    if err != nil {
-        return nil, err
-    }
-    
-    // Threshold signature scheme
-    tpk := bls.Serialize(sk.GetPublicKey())
-    
-    return &ZKThreshold{tks: sk, pk: sk.GetPublicKey()}, nil
+func NewThresholdSig(threshold, total int) (*ThresholdSig, error) {
+	if threshold > total || threshold <= 0 || total <= 0 {
+		return nil, errors.New("invalid threshold parameters")
+	}
+
+	secret, err := bls.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate master key: %w", err)
+	}
+
+	ts := &ThresholdSig{
+		threshold: threshold,
+		total:     total,
+		secret:    secret,
+		pubKey:    secret.GetPublicKey(),
+		shares:    make(map[int]*bls.SecretKey),
+	}
+
+	// Generate shares using Shamir's Secret Sharing
+	for i := 1; i <= total; i++ {
+		share, err := ts.generateShare(i)
+		if err != nil {
+			return nil, err
+		}
+		ts.shares[i] = share
+	}
+
+	return ts, nil
 }
 
-func (zt *ZKThreshold) SignZKProof(txHash []byte, proof zk.Proof) ([]byte, error) {
-    sig := tks.Sign(txHash)
-    return sig.Serialize(), nil
+func (ts *ThresholdSig) Share(index int) *bls.SecretKey {
+	return ts.shares[index]
+}
+
+func (ts *ThresholdSig) PublicKey() *bls.PublicKey {
+	return ts.pubKey
+}
+
+func (ts *ThresholdSig) Sign(message []byte) *bls.Sign {
+	return ts.secret.Sign(message)
+}
+
+func (ts *ThresholdSig) AggregateShares(signatures map[int]*bls.Sign) (*bls.Sign, error) {
+	if len(signatures) < ts.threshold {
+		return nil, fmt.Errorf("insufficient signatures: need %d, got %d", ts.threshold, len(signatures))
+	}
+
+	aggSig := bls.Sign{}
+	for _, sig := range signatures {
+		if err := aggSig.Add(sig); err != nil {
+			return nil, fmt.Errorf("failed to aggregate signature: %w", err)
+		}
+	}
+
+	if !aggSig.Verify(ts.pubKey, []byte("test message")) {
+		return nil, errors.New("aggregated signature verification failed")
+	}
+
+	return &aggSig, nil
+}
+
+func (ts *ThresholdSig) generateShare(index int) (*bls.SecretKey, error) {
+	// Simplified Shamir share generation
+	coeffs := make([]*big.Int, ts.threshold)
+	coeffs[0] = new(big.Int).Set(ts.secret.GetLittleEndian())
+	
+	for i := 1; i < ts.threshold; i++ {
+		coeffs[i], _ = rand.Int(rand.Reader, new(big.Int).Exp(big.NewInt(2), big.NewInt(256), nil))
+	}
+	
+	// Evaluate polynomial at index
+	x := big.NewInt(int64(index))
+	share := new(big.Int)
+	for i, coeff := range coeffs {
+		term := new(big.Int).Exp(x, big.NewInt(int64(i)), nil)
+		term.Mul(term, coeff)
+		term.Mod(term, bls12_381.Q)
+		share.Add(share, term)
+		share.Mod(share, bls12_381.Q)
+	}
+	
+	skBytes := share.Bytes()
+	sk, err := bls.SecretKeyFromBytes(skBytes)
+	return sk, err
 }
